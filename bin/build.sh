6fa255ca8191d191f967f96293d2aeb4f5d06c0f
Jongmin Kim <whdalsrnt@megazone.com>
2021-08-23 17:28:26 +0900

    build: remove manual build commands

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Build a docker image
-DEFAULT=0.1
-VER=${1:-$DEFAULT}
-cd ..
-docker build -t pyengine/plugin-kbfg-identity-auth .
-docker tag pyengine/plugin-kbfg-identity-auth pyengine/plugin-kbfg-identity-auth:${VER}
--- bin/push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# How to upload
-DEFAULT=0.1
-VER=${1:-$DEFAULT}
-./build.sh ${VER}
-docker push pyengine/plugin-kbfg-identity-auth:${VER}
-#docker push spaceone/plugin-kbfg-sso:1.0
